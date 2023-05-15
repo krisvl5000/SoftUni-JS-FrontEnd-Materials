ff413489dad53f674c56c48b61c71168038344a4
Kristian <krisvl5000@gmail.com>
2023-05-15 13:12:14 +0300

    Create solution.js

---
+++ JS Syntax Fundamentals/05. Math Operations/solution.js
@@ -0,0 +1,15 @@
+function calculate (num1, num2, operator){
+    if (operator === '+') {
+        console.log(num1 + num2);
+    } else if (operator === '-') {
+        console.log(num1 - num2);
+    } else if (operator === '*') {
+        console.log(num1 * num2);
+    } else if (operator === '/') {
+        console.log(num1 / num2);
+    } else if (operator === '%') {
+        console.log(num1 % num2);
+    } else if (operator === '**') {
+        console.log(num1 ** num2);
+    }
+}
