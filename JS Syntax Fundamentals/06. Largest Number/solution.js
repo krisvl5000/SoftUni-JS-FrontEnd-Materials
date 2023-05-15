3890ac5263c9ab1f671f11687800e6f8274ffdcb
Kristian <krisvl5000@gmail.com>
2023-05-15 13:32:32 +0300

    Create solution.js

---
+++ JS Syntax Fundamentals/06. Largest Number/solution.js
@@ -0,0 +1,13 @@
+function findLargestNumber (num1, num2, num3){
+    let largestNum = 0;
+
+    if (num1 > num2 && num1 > num3) {
+        largestNum = num1;
+    } else if (num2 > num1 && num2 > num3){
+        largestNum = num2;
+    } else if (num3 > num1 && num3 > num2){
+        largestNum = num3;
+    }
+
+    console.log(largestNum);
+}
