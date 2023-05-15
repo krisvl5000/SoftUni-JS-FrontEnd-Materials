60f418ccf3fe76abdd9f7311a3e22fb0e54a8239
Kristian <krisvl5000@gmail.com>
2023-05-15 12:51:36 +0300

    Create solution.js

---
+++ JS Syntax Fundamentals/03. Excellent Grade/solution.js
@@ -0,0 +1,12 @@
+function checkExcellence (num){
+    if (num >= 5.50) {
+        console.log('Excellent')
+    }
+    else{
+        console.log('Not excellent')
+    }
+}
+
+//Test
+checkExcellence(5.50);
+checkExcellence(5.4999);
