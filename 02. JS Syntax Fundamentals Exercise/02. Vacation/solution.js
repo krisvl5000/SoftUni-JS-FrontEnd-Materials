78792c43214f7ef7daf528626139075e35f96663
Kristian <krisvl5000@gmail.com>
2023-05-17 10:29:04 +0300

    Update solution.js

---
+++ 02. JS Syntax Fundamentals Exercise/02. Vacation/solution.js
@@ -0,0 +1,55 @@
+function priceCalculator (peopleCount, type, dayOfWeek){
+    let price;
+
+    if (dayOfWeek === 'Friday'){
+        if (type === 'Students') {
+            price = 8.45;
+        }
+        else if (type === 'Business'){
+            price = 10.90;
+        }
+        else{
+            price = 15;
+        }
+    }
+    else if (dayOfWeek === 'Saturday'){
+        if (type === 'Students') {
+            price = 9.80;
+        }
+        else if (type === 'Business'){
+            price = 15.60;
+        }
+        else{
+            price = 16;
+        }
+    }
+    else {
+        if (type === 'Students') {
+            price = 10.46;
+        }
+        else if (type === 'Business'){
+            price = 16;
+        }
+        else{
+            price = 22.50;
+        }
+    }
+
+    let totalPrice = price * peopleCount;
+
+    if (peopleCount >= 30) {
+        totalPrice -= totalPrice * 15 / 100;
+    }
+
+    if (peopleCount >= 100) {
+        totalPrice -= price * 10;
+    }
+
+    if (peopleCount >= 10 && peopleCount <= 20) {
+        totalPrice -= totalPrice * 0.1;
+    }
+
+    console.log(`Total price: ${totalPrice.toFixed(2)}`);
+}
+
+priceCalculator(40, 'Regular', 'Saturday');
