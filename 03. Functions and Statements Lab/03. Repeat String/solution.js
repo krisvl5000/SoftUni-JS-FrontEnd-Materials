c4db67c9e6e0fd4620995891d6a380eae622d9ae
Kristian <krisvl5000@gmail.com>
2023-05-18 12:05:36 +0300

    Update solution.js

--- 03. Functions and Statements Lab/03. Repeat String/solution.js
+++ 03. Functions and Statements Lab/03. Repeat String/solution.js
@@ -1,7 +1,7 @@
 function repeatString (stringToRepeat, timesToRepeat){
     let result = stringToRepeat;
 
-    for (let i = 0; i < timesToRepeat; i++) {
+    for (let i = 0; i < timesToRepeat - 1; i++) {
         result += stringToRepeat;
     }
 
