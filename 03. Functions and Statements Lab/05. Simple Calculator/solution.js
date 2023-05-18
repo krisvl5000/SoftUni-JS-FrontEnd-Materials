b8a7cadb81314b2067d94717e04a6434f86fe80c
Kristian <krisvl5000@gmail.com>
2023-05-18 15:19:48 +0300

    Update solution.js

--- 03. Functions and Statements Lab/05. Simple Calculator/solution.js
+++ 03. Functions and Statements Lab/05. Simple Calculator/solution.js
@@ -1,21 +1,35 @@
 function simpleCalculator (num1, num2, operation){
     let result = 0;
 
-    const sum = (a, b) => a + b;
+    const add = (a, b) => a + b;
     const multiply = (a, b) => a * b;
     const subtract = (a, b) => a - b;
     const divide = (a, b) => a / b;
 
-    switch (operation) {
-        case 'add':
-            return sum(num1, num2);
-        case 'subtract':
-            return subtract(num1, num2);   
-        case 'multiply':
-            return multiply(num1, num2);
-        case 'divide':
-            return divide(num1, num2); 
+    // switch (operation) {
+    //     case 'add':
+    //         return add(num1, num2);
+    //     case 'subtract':
+    //         return subtract(num1, num2);   
+    //     case 'multiply':
+    //         return multiply(num1, num2);
+    //     case 'divide':
+    //         return divide(num1, num2); 
+    // }
+
+    // There is also a way to do this without if or switch - we can
+    // define a map, which will contain all anonymous functions, and
+    // call the one that has the same name with the key 
+    //(the name of the operation).
+
+    const operationMap = {
+        add: add,
+        subtract: subtract,
+        divide: divide,
+        multiply: multiply
     }
+
+    return operationMap[operation](num1, num2);
 }
 
 console.log(simpleCalculator(5, 5, 'multiply'));
