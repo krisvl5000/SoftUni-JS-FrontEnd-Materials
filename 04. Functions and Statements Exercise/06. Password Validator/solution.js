843b92bbcde31774ade57b9cb87a2a2e9d98e097
Kristian <krisvl5000@gmail.com>
2023-05-22 10:09:28 +0300

    Update solution.js

--- 04. Functions and Statements Exercise/06. Password Validator/solution.js
+++ 04. Functions and Statements Exercise/06. Password Validator/solution.js
@@ -1,8 +1,18 @@
-function passwordValidator (pass){
+function passwordValidator (password){
     const isValidLength = (pass) => pass.length >= 6 && pass.length <= 10;
     const hasOnlyLettersAndDigits = (pass) => /^[A-Za-z0-9]+&/g.test(pass);
-    if (!isValidLength){
+    const hasAtLeastTwoDigits = (pass) => [...pass.mathAll(/\d/g)].length >= 2;
+
+    let isValid = true;
+
+    if (!isValidLength(password)){
         console.log('Password must be between 6 and 10 characters');
+    }
+    if (!hasOnlyLettersAndDigits(password)) {
+        
+    }
+    if (!hasAtLeastTwoDigits(password)) {
+        
     }
 }
 
