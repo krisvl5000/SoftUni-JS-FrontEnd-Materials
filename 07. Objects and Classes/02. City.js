f46e10bb87cc67c5340b1bf5c3b13c0e91572826
Kristian <krisvl5000@gmail.com>
2023-06-27 12:49:27 +0300

    Update 02. City.js

---
+++ 07. Objects and Classes/02. City.js
@@ -0,0 +1,7 @@
+function city(args) {
+    console.log(`name -> ${args.name}`);
+    console.log(`area -> ${args.area}`);
+    console.log(`population -> ${args.population}`);
+    console.log(`country -> ${args.country}`);
+    console.log(`postCode -> ${args.postCode}`);
+}
