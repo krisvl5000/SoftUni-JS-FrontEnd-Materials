d92e5a63907461ee3b2e06a2bbc190954550bd14
Kristian <krisvl5000@gmail.com>
2023-07-31 12:10:38 +0300

    Update 03. Store Provision.js

--- 07. Objects and Classes Exercise/03. Store Provision.js
+++ 07. Objects and Classes Exercise/03. Store Provision.js
@@ -1,4 +1,4 @@
-function updateStockAndPrint(storeStock, deliveryProducts) {
+function solve(storeStock, deliveryProducts) {
     const stock = {};
 
     for (let i = 0; i < storeStock.length; i += 2) {
@@ -20,19 +20,4 @@ function updateStockAndPrint(storeStock, deliveryProducts) {
     }
 }
 
-solve([
-    'Chips', '5', 'CocaCola', '9', 'Bananas',
-    '14', 'Pasta', '4', 'Beer', '2'
-    ],
-    [
-    'Flour', '44', 'Oil', '12', 'Pasta', '7',
-    'Tomatoes', '70', 'Bananas', '30'
-    ], 
-    [
-        'Salt', '2', 'Fanta', '4', 'Apple', '14',
-        'Water', '4', 'Juice', '5'
-        ],
-        [
-        'Sugar', '44', 'Oil', '12', 'Apple', '7',
-        'Tomatoes', '7', 'Bananas', '30'
-        ]);
+solve();
