c00f20cc9cd3fef463583f6df39c89d9faa4b547
Kristian <krisvl5000@gmail.com>
2023-08-01 13:07:16 +0300

    Update app.js

--- 08. DOM and Events Lab/05. Delete-From-Table/app.js
+++ 08. DOM and Events Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let email = document.getElementsByName('email')[0].value;
+    let secondColumn = document.querySelectorAll(
+        '#customers tr td:nth-child(2)');
+
+        for (const td of secondColumn) {
+            if (td.textContent === email) {
+                let row = td.parentNode;
+                row.parentNode.removeChild(row);
+                document.getElementById('result').textContent = 'Deleted.';
+                return;
+            }
+        }
+
+        document.getElementById('result').textContent = 'Not found.';
 }
