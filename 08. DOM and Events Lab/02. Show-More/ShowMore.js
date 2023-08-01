89a8a73f3448e1f3bbfe97ec502df23afd6297b5
Kristian <krisvl5000@gmail.com>
2023-08-01 11:41:13 +0300

    Update ShowMore.js

--- 08. DOM and Events Lab/02. Show-More/ShowMore.js
+++ 08. DOM and Events Lab/02. Show-More/ShowMore.js
@@ -1,3 +1,8 @@
 function showText() {
-    // TODO
+    const anchor = document.getElementById('more');
+    const text = document.getElementById('text');
+
+    anchor.style.display = 'none';
+    text.style.display = 'inline';
+
 }
