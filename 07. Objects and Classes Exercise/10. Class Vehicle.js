501e5b671cb398c9ddcc704d08293e361cafedfa
Kristian <krisvl5000@gmail.com>
2023-08-01 11:11:59 +0300

    Adding Exercises

---
+++ 07. Objects and Classes Exercise/10. Class Vehicle.js
@@ -0,0 +1,24 @@
+class Vehicle{
+    constructor(type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = {
+        engine: parts.engine,
+        power: parts.power,
+            get quality() {
+                  return this.engine * this.power;
+                },
+            };
+        this.fuel = fuel;
+        }
+
+        drive(fuelloss) {
+            this.fuel -= fuelloss;
+        }
+    }
+
+    let parts = {engine: 6, power: 100};
+    let vehicle = new Vehicle('a', 'b', parts, 200);
+    vehicle.drive(100);
+    console.log(vehicle.fuel);
+    console.log(vehicle.parts.quality);
---
+++ 08. DOM and Events Lab/01. Sum-Numbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+    const firstNum = document.getElementById('num1').value;
+    const secondNum = document.getElementById('num2').value;
+
+    document.getElementById('sum').value = Number(firstNum) + Number(secondNum);
+}
