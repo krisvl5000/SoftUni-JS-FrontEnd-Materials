053104b607a43d83a4fbdd4a9942b4715b070be4
Kristian <krisvl5000@gmail.com>
2023-08-01 17:43:31 +0300

    Update ColorizeTable.js

--- 08. DOM and Events Lab/07. Colorized-Table/ColorizeTable.js
+++ 08. DOM and Events Lab/07. Colorized-Table/ColorizeTable.js
@@ -1,7 +1,7 @@
 function colorize() {
     const tableRows = document.querySelectorAll('table tr');
 
-    for (let i = 0; i < tableRows.length; i+=2) {
+    for (let i = 1; i < tableRows.length; i+=2) {
         tableRows[i].style.backgroundColor = 'Teal';
     }
 }
