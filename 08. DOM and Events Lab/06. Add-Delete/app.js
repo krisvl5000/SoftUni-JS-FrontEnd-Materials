d2e5239eff943410140f789256ac2b42d0c2a25e
Kristian <krisvl5000@gmail.com>
2023-08-01 16:45:30 +0300

    Update app.js

--- 08. DOM and Events Lab/06. Add-Delete/app.js
+++ 08. DOM and Events Lab/06. Add-Delete/app.js
@@ -9,6 +9,16 @@ function addItem() {
     let listItem = document.createElement('li');
     listItem.textContent = newElement;
 
-
+    let remove = document.createElement('a');
+    let linkText = document.createTextNode("[Delete]");
+    remove.appendChild(linkText);
+    remove.href = '#';
+    remove.addEventListener('click', deleteItem);
+    
+    listItem.appendChild(remove);
     list.appendChild(listItem);
+
+    function deleteItem(){
+        listItem.remove();
+    }
 }
