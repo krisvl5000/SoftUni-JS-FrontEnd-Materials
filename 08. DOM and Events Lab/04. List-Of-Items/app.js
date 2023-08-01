a00b92e809e316c30ff60fd6065e52f3957b521e
Kristian <krisvl5000@gmail.com>
2023-08-01 13:07:14 +0300

    Update app.js

--- 08. DOM and Events Lab/04. List-Of-Items/app.js
+++ 08. DOM and Events Lab/04. List-Of-Items/app.js
@@ -1,3 +1,6 @@
 function addItem() {
-    console.log('TODO:...');
+    const text = document.querySelector('#newItemText').value;
+    const li = document.createElement('li');
+    li.textContent = text;
+    document.querySelector('#items').appendChild(li);
 }
