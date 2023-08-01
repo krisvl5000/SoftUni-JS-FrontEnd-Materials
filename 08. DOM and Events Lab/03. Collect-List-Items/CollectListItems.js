a4eb4fd4b9e250372a227f094abcb4975d14264e
Kristian <krisvl5000@gmail.com>
2023-08-01 11:41:14 +0300

    Update CollectListItems.js

--- 08. DOM and Events Lab/03. Collect-List-Items/CollectListItems.js
+++ 08. DOM and Events Lab/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+    const collection = Array.from(document.querySelectorAll('#items li'));
+    const textArea = document.querySelector('#result');
+
+    const text = collection.map((item) => item.textContent).join('\n');
+    textArea.value = text;
 }
