ebbd5a61de6e904cf38d1d7c95403b635d3eda4f
Kristian <krisvl5000@gmail.com>
2023-08-01 17:43:33 +0300

    Update SumTable.js

--- 08. DOM and Events Lab/08. Sum-Table/SumTable.js
+++ 08. DOM and Events Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    const priceItems = Array.from(document.querySelectorAll('td:nth-child(even)'));
+    let sum = 0;
 
+    for (const priceItem of priceItems) {
+        sum += Number(priceItem.textContent);
+    }
+
+    const sumElement = document.getElementById('sum');
+    sumElement.textContent = sum;
 }
