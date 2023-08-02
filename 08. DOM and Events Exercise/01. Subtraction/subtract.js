18060e47aac666e1035cc5494e77c5fb35e3f062
Kristian <krisvl5000@gmail.com>
2023-08-02 09:31:37 +0300

    Update subtract.js

--- 08. DOM and Events Exercise/01. Subtraction/subtract.js
+++ 08. DOM and Events Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstValue = document.querySelectorById('firstNumber').value;
+    const secondValue = document.querySelectorById('secondNumber').value;
+
+    const result = document.getElementById('result');
+
+    result.textContent = Number(firstValue) - Number(secondValue);
 }
