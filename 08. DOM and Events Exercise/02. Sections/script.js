77a356743d93b9dc8782003dd85210e219a460b8
Kristian <krisvl5000@gmail.com>
2023-08-02 11:42:31 +0300

    Update script.js

--- 08. DOM and Events Exercise/02. Sections/script.js
+++ 08. DOM and Events Exercise/02. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   
+   for (const word of words) {
+      const e = document.createElement('div');
+      e.innerHTML = `<p>${word}</p>`;
+      document.getElementById('content').appendChild(e);
+   }
+
+   const paragraphs = Array.from(document.querySelectorAll('p'));
+
+   for (const paragraph of paragraphs) {
+      paragraph.style.display = 'none';
+   }
+
+   const insideDivs = Array.from(document.getElementById('content').children);
+
+   for (const insideDiv of insideDivs) {
+      insideDiv.addEventListener('click', (e) => {
+         e.target.firstChild.style.display = 'block';
+      })
+   }
 }
